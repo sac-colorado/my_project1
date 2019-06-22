51e5de500c754bbb62305fb6e3ddb1e385a9ae10
Sean Chapinm <schapin7@gmail.com>
2019-06-21 18:28:52 -0600

    I have the login and password -- writing to the db working

--- application.py
+++ application.py
@@ -21,17 +21,27 @@ def home_page():
 
 @app.route("/Sign_in", methods=["GET"])
 def goto_login():
-    user_name = request.form.get("user_name")
-    password = request.form.get("password")
-    return render_template("login.html", user_name=user_name, password=password)
+    return render_template("login.html")
 
 @app.route("/register", methods=["GET"])
 def register():
-    user_name = request.form.get("user_name")
-    password = request.form.get("password")
-    return render_template("register.html", user_name=user_name, password=password)
+    return render_template("register.html")
 
-@app.route("/output_user_info", methods=["POST"])
+#To register a new user --> need to check the user name is available    
+@app.route("/save_user_info", methods=["POST"])
+def save_user_info():
+    book_user = request.form.get("user_name")
+    password = request.form.get("password")
+    user_data = db.execute("SELECT * FROM book_users WHERE user_name=(:book_user)",{"book_user": book_user}).fetchall()
+    if user_data ==[]:       #New user is not in the database so add them.
+        db.execute("INSERT INTO book_users (user_name, password) VALUES (:user_name, :password)", {"user_name": book_user, "password": password})
+        db.commit()
+        return render_template("test.html", user_name="Thank you, " + book_user + " -- Welcome to The Book Worm!")
+    else:             #That user name already exists in the database -- try again.
+        return render_template("test.html", user_name="That user name is all ready taken please try to register with a different user_name")
+
+
+""" @app.route("/output_user_info", methods=["POST"])
 def output_user_info():
     book_user = request.form.get("user_name")
     password = request.form.get("password")
@@ -39,5 +49,4 @@ def output_user_info():
     if user_data ==[]:
         return render_template("test.html", user_name="Not a registered User", password=password)
     else:
-        return render_template("test.html", user_name=user_data[0][1], password=password)
-    
+        return render_template("test.html", user_name=user_data[0][1], password=password) """
